7cbd2fc4b8d6c3744c2337881f30df9ef9b774f8
franckkoffi <kkfm2014@gmail.com>
2023-07-16 07:18:19 +0000

    remise

---
+++ cv/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CV KOUADIO FRANCK MARTIAL KOFFI</title>
+  </head>
+  <body>
+    <object data="cv_kouadio_franck_martial_koffi.pdf" type="application/pdf" width="100%" height="700">
+    </object>
+  </body>
+</html>
